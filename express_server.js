30460f7b9a29ee5a56177cfb920eb955d8ae9787
RyanDeMesa <ryandemesa09@gmail.com>
2022-11-29 03:32:32 +0000

    added a new ejs file called urls_new which added a user input form to the urls_new page and created a function in express_server that will generate a random 6 digit alphanumeric string.

--- express_server.js
+++ express_server.js
@@ -9,9 +9,25 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+function generateRandomString() {
+  const random = Math.random().toString(36).slice(2);
+  return random.slice(0, 6);
+}
+
+app.use(express.urlencoded({ extended : true }));
+
+app.post("/urls", (req, res) => {
+ console.log(req.body); // Log the POST request body to the console
+ res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls", (req, res) => {
  const templateVars = { urls: urlDatabase };
  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
