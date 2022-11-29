6faaea4e910c11b4a1a476f23b06700eb0676eaf
RyanDeMesa <ryandemesa09@gmail.com>
2022-11-29 04:47:23 +0000

    added code so that when the user inputs a url a random 6 digit num/letter code will be saved to the urlDatabase as the key and full url saved as the value. Also added code that will redirect the user to the orignial url if the shortened code is pressed

--- express_server.js
+++ express_server.js
@@ -9,6 +9,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+// random string generator
 function generateRandomString() {
   const random = Math.random().toString(36).slice(2);
   return random.slice(0, 6);
@@ -18,7 +19,16 @@ app.use(express.urlencoded({ extended : true }));
 
 app.post("/urls", (req, res) => {
  console.log(req.body); // Log the POST request body to the console
- res.send("Ok"); // Respond with 'Ok' (we will replace this)
+ const shortenedURL = generateRandomString(); // generates random 6 digit code for url inputted
+ urlDatabase[shortenedURL] = req.body.longURL; // addeds shortended url to urlDatabase as key and full url as value
+ res.redirect(`/urls/${shortenedURL}`); // response is to redirect to /urls/:id
+});
+
+//redirects to longURL from shortenedURL
+app.get("/u/:id", (req, res) => {
+  const id = req.params.id
+  const longURL = urlDatabase[id]
+  res.redirect(longURL);
 });
 
 app.get("/urls", (req, res) => {
