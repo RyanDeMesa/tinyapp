38c55c782baa56f435279bf8786e31ad7e864ffd
RyanDeMesa <ryandemesa09@gmail.com>
2022-11-29 04:51:16 +0000

    refactored using eslint

--- express_server.js
+++ express_server.js
@@ -10,30 +10,30 @@ const urlDatabase = {
 };
 
 // random string generator
-function generateRandomString() {
+const generateRandomString = function() {
   const random = Math.random().toString(36).slice(2);
   return random.slice(0, 6);
-}
+};
 
-app.use(express.urlencoded({ extended : true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.post("/urls", (req, res) => {
- console.log(req.body); // Log the POST request body to the console
- const shortenedURL = generateRandomString(); // generates random 6 digit code for url inputted
- urlDatabase[shortenedURL] = req.body.longURL; // addeds shortended url to urlDatabase as key and full url as value
- res.redirect(`/urls/${shortenedURL}`); // response is to redirect to /urls/:id
+  console.log(req.body); // Log the POST request body to the console
+  const shortenedURL = generateRandomString(); // generates random 6 digit code for url inputted
+  urlDatabase[shortenedURL] = req.body.longURL; // addeds shortended url to urlDatabase as key and full url as value
+  res.redirect(`/urls/${shortenedURL}`); // response is to redirect to /urls/:id
 });
 
 //redirects to longURL from shortenedURL
 app.get("/u/:id", (req, res) => {
-  const id = req.params.id
-  const longURL = urlDatabase[id]
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
   res.redirect(longURL);
 });
 
 app.get("/urls", (req, res) => {
- const templateVars = { urls: urlDatabase };
- res.render("urls_index", templateVars);
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -41,8 +41,11 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
- const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
- res.render("urls_show", templateVars);
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/", (req, res) => {
