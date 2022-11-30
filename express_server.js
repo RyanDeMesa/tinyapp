d5c53b5819d39726bc3bebde13dc86cf6d8c8d00
RyanDeMesa <ryandemesa09@gmail.com>
2022-11-30 23:28:13 +0000

    added registration page and a get to express_server to go there when prompted

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 app.set("view engine", "ejs");
 app.use(cookieParser());
@@ -12,12 +12,20 @@ const urlDatabase = {
 };
 
 // random string generator
-const generateRandomString = function() {
+const generateRandomString = function () {
   const random = Math.random().toString(36).slice(2);
   return random.slice(0, 6);
 };
 
 app.use(express.urlencoded({ extended: true }));
+
+// page for registation
+app.get("/register/", (req,res) => {
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("register", templateVars);
+})
 
 // page clears cookies and redirects back to /urls/
 app.post("/logout/", (req, res) => {
@@ -29,11 +37,11 @@ app.post("/logout/", (req, res) => {
 // this page saves the username to cookies then redirects back to /urls/
 app.post("/login/", (req, res) => {
   const username = req.body.username;
-  res.cookie('username', username);
+  res.cookie("username", username);
   res.redirect("/urls/");
 });
 
-// creates new short url when user submits form with long url then redirects to show page 
+// creates new short url when user submits form with long url then redirects to show page
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   const shortenedURL = generateRandomString(); // generates random 6 digit code for url inputted
@@ -48,18 +56,20 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
-// page that shows all urls 
+// page that shows all urls
 app.get("/urls", (req, res) => {
-  const templateVars = { 
+  const templateVars = {
     urls: urlDatabase,
-    username: req.cookies['username']
+    username: req.cookies["username"],
   };
   res.render("urls_index", templateVars);
 });
 
 // Page that lets you add new urls
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let username = req.cookies["username"]
+  const templateVars = {username};
+  res.render("urls_new", templateVars);
 });
 
 // Shows page for short url
@@ -67,7 +77,7 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
-    username: req.cookies['username']
+    username: req.cookies["username"],
   };
   res.render("urls_show", templateVars);
 });
@@ -79,13 +89,13 @@ app.get("/", (req, res) => {
 
 // when url is deleted this removes it from urlDatabase and redirects user to /urls page
 app.post("/urls/:id/delete", (req, res) => {
-  const id = req.params.id
-  delete urlDatabase[id]
-  res.redirect('/urls')
-})
+  const id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect("/urls");
+});
 
 // Redirects back to /url/ which is now updated with new longURL
-app.post('/urls/:id', (req, res) => {
+app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect("/urls/");
 });
